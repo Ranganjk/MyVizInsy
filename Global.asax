75c56063503546a6f5cbbfa104d3c542ec66a5ff
Ranganjk <51149328+Ranganjk@users.noreply.github.com>
2022-04-22 13:54:09 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NerolacPreviewApp.MvcApplication" Language="C#" %>
---
+++ GridPaging.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Specialized;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+
+namespace NerolacPreviewApp
+{
+    public static class GridPaging
+    {
+
+        public static IHtmlString SortIdentifier(this HtmlHelper htmlHelper, string sortOrder, string field)
+        {
+            if (string.IsNullOrEmpty(sortOrder) || (sortOrder.Trim() != field && sortOrder.Replace("_desc", "").Trim() != field)) return null;
+
+            string glyph = "glyphicon glyphicon-chevron-up";
+            if (sortOrder.ToLower().Contains("desc"))
+            {
+                glyph = "glyphicon glyphicon-chevron-down";
+            }
+
+            var span = new TagBuilder("span");
+            span.Attributes["class"] = glyph;
+
+            return MvcHtmlString.Create(span.ToString());
+        }
+
+        public static RouteValueDictionary ToRouteValueDictionary(this NameValueCollection collection, string newKey, string newValue)
+        {
+            var routeValueDictionary = new RouteValueDictionary();
+            foreach (var key in collection.AllKeys)
+            {
+                if (key == null) continue;
+                if (routeValueDictionary.ContainsKey(key))
+                    routeValueDictionary.Remove(key);
+
+                routeValueDictionary.Add(key, collection[key]);
+            }
+            if (string.IsNullOrEmpty(newValue))
+            {
+                routeValueDictionary.Remove(newKey);
+            }
+            else
+            {
+                if (routeValueDictionary.ContainsKey(newKey))
+                    routeValueDictionary.Remove(newKey);
+
+                routeValueDictionary.Add(newKey, newValue);
+            }
+            return routeValueDictionary;
+        }
+       
+
+    }
+}
